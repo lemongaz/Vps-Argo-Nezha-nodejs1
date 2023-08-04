ca0d1500c09e5d2167dea9af3a4b943d99c865cb
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-08-05 00:37:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -443,9 +443,7 @@ echo "==========================================================================
 }
 
 install_bbr(){
-[ ! -s ./tcp.sh ] && wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh"
-chmod +x ./tcp.sh
-./tcp.sh
+bash <(curl -Lso- https://git.io/kernel.sh)
 }
 
 start_menu1(){
