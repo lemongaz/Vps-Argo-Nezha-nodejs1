8d8d2b61748e1dfd5975e0e29eb07abcb96edde1
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-08-05 10:11:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -435,7 +435,7 @@ echo "==========================================================================
 echo "        "
 [ "$(systemctl is-active naray)" = 'active' ] && echo "X-RA-Y安装成功!等待一会看隧道亮不亮，如果不亮可重启试试"
 
-[ "$(systemctl is-active naray)" != 'active' ] && echo "X-RA-Y安装失败!请重新安装或者重启试试"
+[ "$(systemctl is-active naray)" != 'active' ] && echo "X-RA-Y安装失败!请重新安装或更换端口或者重启试试"
 echo "         "
 echo "输入域名/list查看默认节点信息，如果需要更改UUID等信息可以替换URL_BOT地址 "
 echo "         "
